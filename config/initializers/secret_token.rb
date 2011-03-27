092c25c9935724bf142213a48cf5f71818d1580f
Purposeful1 <pds@u.northwestern.edu>
2011-03-27 01:57:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fivethings::Application.config.secret_token = '43718eef008ffade1c5755d97ea9384c74d0d23174e88b06b266defd01df7889842f455e0bf8e776edda27dcf602c709f3850d9bd8de0a503c44418b3ec87a10'
